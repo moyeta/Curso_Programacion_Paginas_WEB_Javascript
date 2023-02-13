39bc7ed3b9e47e5b58067d8e402fbf94e32ce7d0
moyeta <moya.geologo@gmail.com>
2023-02-13 20:04:01 +0100

    Add files via upload

---
+++ JAVASCRIPT_I/ejemplo3-Funciones_I/js/script3.js
@@ -0,0 +1,9 @@
+/*
+Cómo método su sintaxis: .getElementById('nombreId')
+Definido como LA PROYECCIÓN DE UN ELEMENTO QUE CONTENDRÁ LA DEFINICIÓN
+QUE TRASLADA EL id
+EN definitiva poder manipular u obtener información de un elemento y su
+contenido dentro del DocType
+id- referenciador que html para conectar el documento desde otros entornos
+
+*/
